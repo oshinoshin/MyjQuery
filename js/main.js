d148593545e48c603cfc435b5d1e5841b1a4241e
shinyatoda <donateluro@gmail.com>
2021-12-07 18:00:35 +0900

    create input

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
 $(() => {
-  
+  const $input = $('input');
+  $input.focus();
+
+  $('button').click(() => {
+    $('<li>')
+      .text($input.val())
+      .appendTo('ul');
+    $input
+      .val('')
+      .focus();
+  });
+
+  $('ul').click( e => {
+    if (e.target.nodeName !== 'LI' || !confirm('are you sure?')) {
+      return;
+    } 
+    $(e.target).fadeOut();
+  });
 });
